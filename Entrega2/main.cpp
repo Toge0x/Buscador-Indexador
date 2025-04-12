b305d5846f20e0fe714db33c848cd50cd7b2372a
amsm30-ua <amsm30@gcloud.ua.es>
2025-04-12 17:17:25 +0200

    2 tests pasados y stopwords cortos falta indexar

--- Entrega2/main.cpp
+++ Entrega2/main.cpp
@@ -2,23 +2,54 @@
 #include <string>
 #include <list> 
 #include "indexadorHash.h"
+#include "indexadorInformacion.h"
 
 using namespace std;
 
 int
 main(void)
 {
-IndexadorHash a("./StopWordsEspanyol.txt", ". ,:", true, true, "./indicePrueba", 1, true);
-cout << "IndexadorHash a:\n################\n" << a << endl;
+IndexadorHash a("./StopWordsEspanyol.txt", ". ,:", false, false, "./indicePrueba", 0, true);
 
-IndexadorHash b("./StopWordsIngles.txt", ". ,:", true, false, "./indicePruebaIngles", 2, true);
-cout << "IndexadorHash b:\n################\n" << b << endl;
+a.Indexar("./listaFicheros_corto.txt");
 
-cout << b.DevolverDelimitadores () << endl; 
-cout << b.DevolverDirIndice () << endl; 
-cout << b.DevolverTipoStemming () << endl; 
-cout << b.DevolverAlmacenarPosTerm () << endl; 
-cout << b.DevolverPasarAminuscSinAcentos () << endl; 
-cout << b.DevolverCasosEspeciales () << endl; 
+InformacionTermino inf1;
+
+if(a.Devuelve("pal1", inf1))
+	cout << "pal1 SE HA INDEXADO: " << inf1 << endl;
+else
+	cout << "pal1 NO SE HA INDEXADO" << endl;
+
+if(a.Devuelve("pal7", inf1))
+	cout << "pal7 SE HA INDEXADO: " << inf1 << endl;
+else
+	cout << "pal7 NO SE HA INDEXADO" << endl;
+
+InfTermDoc infDoc1;
+
+if(a.Devuelve("pal1", "corpus_corto/fichero1.txt", infDoc1))
+	cout << "pal1 SE HA INDEXADO EN corpus_corto/fichero1.txt: " << infDoc1 << endl;
+else
+	cout << "pal1 NO SE HA INDEXADO EN corpus_corto/fichero1.txt" << endl;
+
+if(a.Devuelve("pal1", "fichero1.txt", infDoc1))
+	cout << "pal1 SE HA INDEXADO EN fichero1.txt: " << infDoc1 << endl;
+else
+	cout << "pal1 NO SE HA INDEXADO EN fichero1.txt" << endl;
+
+if(a.Devuelve("pal7", "corpus_corto/fichero1.txt", infDoc1))
+	cout << "pal7 SE HA INDEXADO EN corpus_corto/fichero1.txt: " << infDoc1 << endl;
+else
+	cout << "pal7 NO SE HA INDEXADO EN corpus_corto/fichero1.txt" << endl;
+
+if(a.Existe("pal1"))
+	cout << "pal1 SE HA INDEXADO" << endl;
+else
+	cout << "pal1 NO SE HA INDEXADO" << endl;
+
+if(a.Existe("pal7"))
+	cout << "pal7 SE HA INDEXADO" << endl;
+else
+	cout << "pal7 NO SE HA INDEXADO" << endl;
 
 }
